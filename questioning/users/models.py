5897733ca0f8e369bf28cf83429bfc71f2f6d01b
kk <705555262@qq.com>
2019-07-27 22:42:46 +0800

    完成用户模型

--- questioning/users/models.py
+++ questioning/users/models.py
@@ -1,14 +1,38 @@
+from __future__ import unicode_literals
+
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
 
 
+@python_2_unicode_compatible
 class User(AbstractUser):
 
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    nickname = models.CharField(null=True, blank=True, max_length=255, verbose_name='昵称')
+    job_title = models.CharField(max_length=50, null=True, blank=True, verbose_name='职称')
+    introduction = models.TextField(blank=True, null=True, verbose_name='简介')
+    picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True, verbose_name='头像')
+    location = models.CharField(max_length=50, null=True, blank=True, verbose_name='城市')
+    personal_url = models.URLField(max_length=255, blank=True, null=True, verbose_name='个人链接')
+    weibo = models.URLField(max_length=255, blank=True, null=True, verbose_name='微博链接')
+    zhihu = models.URLField(max_length=255, blank=True, null=True, verbose_name='知乎链接')
+    github = models.URLField(max_length=255, blank=True, null=True, verbose_name='Github链接')
+    linkedin = models.URLField(max_length=255, blank=True, null=True, verbose_name='LinkedIn链接')
+
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
 
     def get_absolute_url(self):
-        return reverse("users:detail", kwargs={"username": self.username})
+        return reverse('users:detail', kwargs={
+            'username': self.username
+        })
+
+    def get_profile_name(self):
+        if self.nickname:
+            return self.nickname
+        return self.username
