c3eefb8c59c26942b699a53b819e3bc5bd0d009c
kk <705555262@qq.com>
2019-08-26 12:54:28 +0800

    完成充值模型设计

---
+++ questioning/trade/__init__.py
@@ -0,0 +1,7 @@
+ORDER_STATUS = (
+    ('TRADE_SUCCESS', '交易支付成功'),
+    ('TRADE_CLOSED', '未付款交易超时关闭'),
+    ('WAIT_BUYER_PAY', '交易创建'),
+    ('TRADE_FINISHED', '交易结束'),
+    ('paying', '待支付'),
+)
--- questioning/trade/models.py
+++ questioning/trade/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from django.utils.encoding import python_2_unicode_compatible
+
+from questioning.trade import ORDER_STATUS
+from questioning.utils.models import CreatedUpdatedMixin
+
+@python_2_unicode_compatible
+class OrderInfo(CreatedUpdatedMixin, models.Model):
+    '''
+    充值订单详情
+    '''
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='用户')
+    order_sn = models.CharField(max_length=30, null=True, blank=True, unique=True, verbose_name='订单号')
+    trade_no = models.CharField(max_length=100, unique=True, null=True, blank=True, verbose_name='支付订单号')
+
+    pay_status = models.CharField(choices=ORDER_STATUS, max_length=40, verbose_name='订单状态', default='paying')
+    order_mount = models.DecimalField(verbose_name="充值金额", max_digits=10,
+                                decimal_places=2, default=0.00)
+    pay_time = models.DateTimeField(null=True, blank=True, verbose_name="支付时间")
+
+    class Meta:
+        verbose_name = "充值订单"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.order_sn
